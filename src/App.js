5b0f91d738e6d450d6ce01e1114c121688242589
luiso-o <cheportillo@gmail.com>
2023-09-15 16:59:14 +0200

    version 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+function realizarSuma(e) {
+  e.preventDefault();
+  const v1 = parseInt(e.target.valor1.value);
+  const v2 = parseInt(e.target.valor2.value);
+  const suma = v1 + v2;
+  const resta = v1 - v2;
+  alert('La suma es : ' + suma);
+  alert('La resta es : ' + resta);
+}
+
 function App() {
+  const estiloApp = {
+    backgroundColor: 'lightblue', 
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className = "contenedor-centrado" style={estiloApp}>
+      <form onSubmit={realizarSuma}>
+        <p className="texto-azul"> Ingrese el primer valor : 
+        <input type="number" name="valor1" />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <p className="texto-rojo">Ingrese el segundo valor :
+        <input type="number" name="valor2" />
+        </p>
+
+        <p>
+          <input type="submit" name="sumar" className="boton-verde" />
+        </p>
+      </form>
     </div>
   );
 }
